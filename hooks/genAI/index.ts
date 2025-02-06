68f9e36e119fe9fc5f4bca6a80be0cfd45c57054
Lexy <laxman.rai.07.26@gmail.com>
2025-02-06 07:38:18 -0500

    feat:tg bot works

---
+++ hooks/genAI/index.ts
@@ -0,0 +1,9 @@
+// import the Genkit and Google AI plugin libraries
+import { gemini15Flash, googleAI } from '@genkit-ai/googleai';
+import { genkit } from 'genkit';
+
+// configure a Genkit instance
+export const ai = genkit({
+  plugins: [googleAI()],
+  model: gemini15Flash, // set default model
+});
--- hooks/telegramBot/index.ts
+++ hooks/telegramBot/index.ts
@@ -1,34 +1,22 @@
-import { Telegraf, Context } from 'telegraf';
-import {chat} from '../genkit/index';
-
-// Define a custom context with additional properties
-interface MyContext extends Context {
-  userInfo?: {
-    firstName: string;
-    lastName: string;
-  };
-}
+import { Telegraf } from 'telegraf';
+import { ai } from '../genAI/index'
 
 // Initialize the bot with your token
-const bot = new Telegraf<MyContext>(process.env.TELEGRAM_BOT_TOKEN || "");
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN || "");
 
-// Handle the /start command
-bot.start((ctx) => {
-  ctx.userInfo = { firstName: 'John', lastName: 'Doe' }; // Example of adding custom properties
-  return ctx.reply(`Welcome, ${ctx.userInfo.firstName}!`);
-});
+// // Handle the /start command
+// bot.start((ctx) => {
+//   ctx.userInfo = { firstName: 'John', lastName: 'Doe' }; // Example of adding custom properties
+//   return ctx.reply(`Welcome, ${ctx.userInfo.firstName}!`);
+// });
 
 // Handle a simple text message
-bot.on('text', (ctx) => {
+bot.on('text', async (ctx) => {
   if (ctx) {
     // Sample interaction
-    (async () => {
-      // Example conversation where user asks about crypto trading
-      const simpleResponse = await chat.send('hello');
-      console.log(simpleResponse);
+    const { text } = await ai.generate(ctx.message.text);
 
-      return ctx.reply('Hello, you sent a message!');
-    })();
+    return ctx.reply(text);
   }
 });
 
